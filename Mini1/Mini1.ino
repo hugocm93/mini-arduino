5439df7d058d7858074eb69afef06be5f62705d9
Hugo Costa Machado <hugocm93@gmail.com>
2016-04-05 20:28:23 -0300

    Bluetooth module is working

--- Mini1/Mini1.ino
+++ Mini1/Mini1.ino
@@ -1,14 +1,14 @@
-/*Nesse mini-projeto, eu controlaria esse LED verde de acordo
-  com a temperatura ou de de acordo com um timer estático. O push-button
+/*Nesse mini-projeto, eu controlaria esse LED de acordo
+  com a temperatura ou de acordo com um timer estático. O push-button
   e o LED RGB auxiliariam ao usuário escolher como vai ser controlado o
-  LED verde. Ou pela temperatura ou por um timer fixo. Para o projeto
+  LED. Ou pela temperatura ou por um timer fixo. Para o projeto
   final, eu usaria o relay com o ventilador, e o teclado numérico para
   definir um tempo personalizado, e o display para exibir opções e exibir
-  a contagem regressiva do cronômetro. Extra: Threads*/
+  a contagem regressiva do cronômetro. Extra: Threads e Bluetooth*/
 
 #include<string.h>
 
-#define GREEN_LED 13
+#define LED 13
 #define RGB_LED_R 12
 #define RGB_LED_G 11
 #define RGB_LED_B 10
@@ -17,22 +17,26 @@
 #define BUTTON1 2
 
 #define INTERVAL 3000
-#define TIMEOUT 60000
-#define THRESHOLD 45
+#define TIMEOUT  10000
+#define THRESHOLD 27.0
 
 unsigned long lastUpdateTime;
+unsigned long runtime;
 int oldValue;
 int option;
-int runtime;
 bool lastState;
 bool currentState;
 
+char junk;
+String inputString = "";
+
 void rgb(char* color);
 void buttonChanged(bool state);
+float tempInCelsius(int sensorValue);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(GREEN_LED, OUTPUT);
+  pinMode(LED, OUTPUT);
   pinMode(RGB_LED_R, OUTPUT);
   pinMode(RGB_LED_G, OUTPUT);
   pinMode(RGB_LED_B, OUTPUT);
@@ -42,9 +46,9 @@ void setup() {
   oldValue = analogRead(SENSOR);
   lastUpdateTime = millis();
   option = -1;
-  runtime = -1;
+  runtime = 0;
   lastState = LOW;
-  state = LOW;
+  currentState = LOW;
 }
 
 void loop() {
@@ -60,34 +64,57 @@ void loop() {
     buttonChanged(aux);
   }
 
-  Serial.println(option);
+  if (Serial.available()) {
+    inputString = "";
+    while (Serial.available())
+    {
+      char inChar = (char)Serial.read(); //read the input
+      if (inChar != ' ') {
+        inputString += inChar;        //make a string of the characters coming on serial
+      }
+    }
+    Serial.println(inputString);
+    while (Serial.available() > 0)
+    {
+      junk = Serial.read() ;  // clear the serial buffer
+    }
+    if (inputString == "1") {       //in case of 'a' turn the LED on
+      buttonChanged(HIGH);
+    }
+  }
+
+  //Serial.println(option);
 
   switch (option) {
     case 1:
-      rgb("red");
-      if (oldValue > THRESHOLD) {
-        digitalWrite(GREEN_LED, HIGH);
+      rgb("green");
+      Serial.print("Temperature ");
+      Serial.println(tempInCelsius(oldValue));
+      if (tempInCelsius(oldValue) > THRESHOLD) {
+        digitalWrite(LED, HIGH);
       }
       else {
-        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(LED, LOW);
       }
       break;
 
     case 2:
       rgb("blue");
-      if (runtime > TIMEOUT + millis()) {
-        digitalWrite(GREEN_LED, LOW);
+      Serial.print("Timer ");
+      Serial.println(millis() - runtime);
+      if (millis() - runtime > TIMEOUT) {
+        digitalWrite(LED, LOW);
         option = -1;
       }
       else {
-        digitalWrite(GREEN_LED, HIGH);
+        digitalWrite(LED, HIGH);
       }
       break;
 
     default:
-      rgb("green");
+      rgb("red");
+      runtime = 0;
   }
-
 }
 
 void rgb(char* color) {
@@ -112,15 +139,20 @@ void buttonChanged(bool state) {
   if (state) {
     if (currentState) {
       option = 1;
-      runtime = -1;
+      runtime = 0;
     }
     else {
       option = 2;
-      if (runtime == -1) {
+      if (runtime == 0) {
         runtime = millis();
       }
     }
     currentState = !currentState;
   }
+}
+
+float tempInCelsius(int sensorValue) {
+  float mV = (sensorValue) * (5000 / 1024.0);
+  return (mV) / 10.;
 }
 
