02f398dde7a82f6cec71e2a03172619c94180f1b
Hugo Costa Machado <hugocm93@gmail.com>
2016-04-18 22:14:56 -0300

    Presentation

--- Mini1/Mini1.ino
+++ Mini1/Mini1.ino
@@ -4,38 +4,71 @@
   LED. Ou pela temperatura ou por um timer fixo. Para o projeto
   final, eu usaria o relay com o ventilador, e o teclado numérico para
   definir um tempo personalizado, e o display para exibir opções e exibir
-  a contagem regressiva do cronômetro. Extra: Threads e Bluetooth*/
+  a contagem regressiva do cronômetro. Extra: Bluetooth*/
 
-#include<string.h>
-
+/*Led branco que está representando o relay*/
 #define LED 13
+
+/*Led RGB que sinaliza em qual modo está o programa:
+  modo temperatura, modo timer ou modo idle.*/
 #define RGB_LED_R 12
 #define RGB_LED_G 11
 #define RGB_LED_B 10
 
+/*Sensor que lê a temperatura*/
 #define SENSOR A0
+
+/*Botão que coloca o programa em modo temperatura, timer ou idle.*/
 #define BUTTON1 2
 
+/*Primeiro: intervalo em milisegundos entre as checagens de temperatura.
+  Segundo: segundos do timer. Terceiro: Temperatura limiar.*/
 #define INTERVAL 3000
 #define TIMEOUT  10000
 #define THRESHOLD 27.0
 
+unsigned long interval = INTERVAL, timeout = TIMEOUT;
+float threshold = THRESHOLD;
+
+/*Guarda a última vez que foi feita a atualização da temperatura*/
 unsigned long lastUpdateTime;
-unsigned long runtime;
-int oldValue;
+
+/*Guarda o tempo que o timer começou a contar. timerStamp == 0
+  significa que ele não foi inicializado*/
+unsigned long timerStamp;
+
+/*Guarda a última leitura do sensor de temperatura*/
+int oldSensorValue;
+
+/*Opção do modo Temperatura ou modo Timer. option == -1 significa que
+  ele não foi inicializado*/
 int option;
-bool lastState;
-bool currentState;
 
-char junk;
-String inputString = "";
+/*Último estado do botão*/
+bool lastButtonState;
 
+/*Modo corrente do programa. Dois modos possíveis*/
+bool currentMode;
+
+/*Limpa todos os estados do programa*/
+void clearAllStates();
+
+/*Acende o led RGB de acordo com a cor desejada("red", "green" ou "blue")*/
 void rgb(char* color);
+
+/*Função que é chamada quando o botão tem o seu estado modificado*/
 void buttonChanged(bool state);
+
+/*Função que converte a temperatura para Celsius*/
 float tempInCelsius(int sensorValue);
+
+/*Função que verifica se algo foi recebido no Serial*/
+void receiveData();
 
 void setup() {
   Serial.begin(9600);
+
+  /*Define cada porta*/
   pinMode(LED, OUTPUT);
   pinMode(RGB_LED_R, OUTPUT);
   pinMode(RGB_LED_G, OUTPUT);
@@ -43,54 +76,35 @@ void setup() {
   pinMode(SENSOR, INPUT);
   pinMode(BUTTON1, INPUT);
 
-  oldValue = analogRead(SENSOR);
-  lastUpdateTime = millis();
-  option = -1;
-  runtime = 0;
-  lastState = LOW;
-  currentState = LOW;
+  clearAllStates();
 }
 
 void loop() {
-  /*Somente atualiza de tempos em tempos*/
-  if (millis() >= lastUpdateTime + INTERVAL) {
-    oldValue = analogRead(SENSOR);
+  /*Verifica a temperatura de tempos em tempos*/
+  if (millis() >= lastUpdateTime + interval) {
+    oldSensorValue = analogRead(SENSOR);
     lastUpdateTime = millis();
   }
 
-  bool aux = digitalRead(BUTTON1);
-  if (aux != lastState) {
-    lastState = aux;
-    buttonChanged(aux);
+  /*Verifica se o botão mudou de estado.*/
+  bool buttonRead = digitalRead(BUTTON1);
+  if (buttonRead != lastButtonState) {
+    lastButtonState = buttonRead;
+    buttonChanged(buttonRead);
   }
 
-  if (Serial.available()) {
-    inputString = "";
-    while (Serial.available())
-    {
-      char inChar = (char)Serial.read(); //read the input
-      if (inChar != ' ') {
-        inputString += inChar;        //make a string of the characters coming on serial
-      }
-    }
-    Serial.println(inputString);
-    while (Serial.available() > 0)
-    {
-      junk = Serial.read() ;  // clear the serial buffer
-    }
-    if (inputString == "1") {       //in case of 'a' turn the LED on
-      buttonChanged(HIGH);
-    }
-  }
+  /*Recebe dados vindos da comunicação serial*/
+  receiveData();
 
-  //Serial.println(option);
-
+  /*Tratamento e comportamento de cada modo*/
   switch (option) {
     case 1:
       rgb("green");
       Serial.print("Temperature ");
-      Serial.println(tempInCelsius(oldValue));
-      if (tempInCelsius(oldValue) > THRESHOLD) {
+      Serial.println(tempInCelsius(oldSensorValue));
+
+      /*Verifica se a temperatura limiar foi atingida e faz a ação em caso afirmativo*/
+      if (tempInCelsius(oldSensorValue) > threshold) {
         digitalWrite(LED, HIGH);
       }
       else {
@@ -101,8 +115,10 @@ void loop() {
     case 2:
       rgb("blue");
       Serial.print("Timer ");
-      Serial.println(millis() - runtime);
-      if (millis() - runtime > TIMEOUT) {
+      Serial.println(millis() - timerStamp);
+
+      /*Verifica se o timer foi atingido e faz a ação em caso afirmativo*/
+      if (millis() - timerStamp > timeout) {
         digitalWrite(LED, LOW);
         option = -1;
       }
@@ -113,7 +129,7 @@ void loop() {
 
     default:
       rgb("red");
-      runtime = 0;
+      timerStamp = 0;
   }
 }
 
@@ -137,22 +153,84 @@ void rgb(char* color) {
 
 void buttonChanged(bool state) {
   if (state) {
-    if (currentState) {
+    if (currentMode) {
       option = 1;
-      runtime = 0;
+      timerStamp = 0;
     }
     else {
       option = 2;
-      if (runtime == 0) {
-        runtime = millis();
+      if (timerStamp == 0) {
+        timerStamp = millis();
       }
     }
-    currentState = !currentState;
+    currentMode = !currentMode;
   }
 }
 
 float tempInCelsius(int sensorValue) {
   float mV = (sensorValue) * (5000 / 1024.0);
   return (mV) / 10.;
+}
+
+void clearAllStates() {
+  oldSensorValue = analogRead(SENSOR);
+  lastUpdateTime = millis();
+  option = -1;
+  timerStamp = 0;
+  lastButtonState = LOW;
+  currentMode = LOW;
+  digitalWrite(LED, LOW);
+}
+
+void receiveData() {
+  if (Serial.available()) {
+
+    String inputString = "";
+
+    /*Lê do serial se disponível e constroi a String*/
+    while (Serial.available()) {
+      char inChar = (char)Serial.read();
+      inputString += inChar;
+    }
+
+    /*Limpa o buffer*/
+    while (Serial.available()) {
+      Serial.read() ;
+    }
+
+    /*Avalia o que foi lido e faz as ações correspondentes*/
+    if (inputString == "1") {
+      buttonChanged(HIGH);
+    }
+    else if (inputString == "0") {
+      clearAllStates();
+    }
+
+    /*Ainda não funciona*/
+    else if (inputString == "setup" || inputString == "SETUP") {
+      clearAllStates();
+      bool flag = true;
+      while (flag) {
+        String inputString[3] = {"","",""};
+        int i = 0;
+        if (Serial.available()) {
+          while (Serial.available()) {
+            char inChar = (char)Serial.read();
+            inputString[i] += inChar;
+            if(inChar == ' '){
+              i++;
+            }
+            if(inputString[0] != "" && inputString[1] != "" && inputString[2] != ""){
+              Serial.println(inputString[0]);
+              Serial.println(inputString[1]);
+              Serial.println(inputString[2]);
+              flag = false;
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
--- PROJETO.md
+++ PROJETO.md
@@ -1,19 +1,19 @@
 
 Projeto Final
 
-Pensei em fazer um projeto usando um sensor de temperatura, um teclado numérico, um display, alguns push-buttons e um relay para controlar um ventilador. A idéia é criar um timer para desligar o ventilador automaticamente. O teclado receberia o tempo em segundos, e o display mostraria o count-down e eventuais mensagens para o usuário navegar no menu. No menu também teria a opção de desligar o ventilador automaticamente conforme a temperatura ambiente. Os push-buttons seriam para escolher a função. As funções poderam ser controladas por bluetooth.
+Pensei em fazer um projeto usando um sensor de temperatura, um teclado numérico, um display, alguns push-buttons e um relay para controlar um ventilador. A idéia é criar um timer para desligar o ventilador automaticamente. O teclado receberia o tempo em segundos, e o display mostraria o count-down e eventuais mensagens para o usuário navegar no menu. No menu também teria a opção de desligar o ventilador automaticamente conforme a temperatura ambiente. Os push-buttons seriam para escolher a função. As funções poderão ser controladas por bluetooth.
 
 
 Proposta de Mini-projeto
 
-Visto que projeto final vai ser uma extensão de um mini-projeto, pensei em fazer o seguinte para esse primeiro mini-projeto: usar um sensor de temperatura, um LED RGB, um push-button e um LED verde. O LED verde seria uma simplificação do relay. Nesse mini-projeto, eu controlaria esse LED verde de acordo com a temperatura ou de de acordo com um timer estático. O push-button e o LED RGB auxiliariam ao usuário escolher como vai ser controlado o LED verde. Ou pela temperatura ou por um timer fixo.
+Visto que projeto final vai ser uma extensão de um mini-projeto, pensei em fazer o seguinte para esse primeiro mini-projeto: usar um sensor de temperatura, um LED RGB, um push-button e um LED vermelho. O LED vermelho seria uma simplificação do relay. Nesse mini-projeto, eu controlaria esse LED vermelho de acordo com a temperatura ou de de acordo com um timer estático. O push-button e o LED RGB auxiliariam ao usuário escolher como vai ser controlado o LED vermelho. Ou pela temperatura ou por um timer fixo.
 Para o projeto final, eu usaria o relay com o ventilador, e o teclado numérico para definir um tempo personalizado, e o display para exibir opções e exibir a contagem regressiva do cronômetro.
 
 
 Sensores: Sensor de temperatura
 
-Atuadores: LED RGB; Push-button; LED verde
+Atuadores: LED RGB; Push-button; LED vermelho
 
 Implementação: C
 
-Extra: Threads
+Extra: Bluetooth
